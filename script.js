24cad809d013cb9a88d53335649fd115c071d051
sauloads15 <saulosmithads15@gmail.com>
2024-07-18 12:20:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,165 @@
+//function autenticarUsuario(){
+   // let senha= prompt('Digite a senha:')
+    //let codigo= prompt('Digite o código:')
+    //if(senha === '123' && codigo === '123'){
+//alert('autenticado')
+//}
+//else{
+  //  alert('Falha na autenticação')
+//}
+//}
+//autenticarUsuario()
+// fazer o pix
+//autenticarUsuario()
+
+// fazer recarga
+//autenticarUsuario()
+
+//function verificarNumero() {
+  //  let numero = Number(prompt('Digite um número: '))
+    //if (numero > 0) {
+     //   alert('Positivo')
+   // }
+   // else {
+    //    alert('Negativo')
+//    }
+//}
+
+//verificarNumero()
+
+//Escreva uma função que lê um número e informa se ele é par ou ímpar
+
+//function verificarNumero() {
+    //let num = Number(prompt('Digite um número: '))
+    //if (num % 2 === 0) {
+      //  alert('par')
+    //}
+    //else {
+    //    alert('ímpar')
+  //  }
+//}
+
+//verificarNumero()
+
+//Escreva uma função que lê a velocidade de um veiculo e informe se ele está acima ou dentro do limite (limite = 50)
+
+//function verificarVelocidade() {
+  //  let velocidade = Number(prompt('Digite a velocidade: '))
+  //  if (velocidade > 50) {
+   //     alert('Velocidade acima do limite')
+   // }
+   // else {
+    //    alert('Velocidade dentro do limite')
+    //}
+//}
+
+//verificarVelocidade()
+
+//Escreva uma função que lê a distancia percorrida em uma viagem e informa quando de combustivel ele gastou nessa viagem considerando que o veiculo faz 10km/l
+
+//function calcularConsumo() {
+    //let distancia = Number(prompt('Digite a distancia da viagem: '))
+    //let consumo = distancia / 10
+  //  alert(`Consumo da viagem: ${consumo}`)    
+//}
+
+//calcularConsumo()
+
+//Escreva uma função que lê um valor de horas inteiro e informa quantos minutos e segundos esse valor tem
+
+//function converterHoras() {
+    //let horas = Number(prompt('Informe a quantidade de horas: '))
+    //let minutos = horas * 60
+    //let segundos = horas * 3600
+    //alert(`Quantidade de minutos: ${minutos}`)
+  //  alert(`Quantidade de segundos: ${segundos}`)
+//}
+
+//converterHoras()
+
+//function calcularSalario(horasTrabalhadas, valor, comissao){
+//let salario = horasTrabalhadas * valor + comissao
+//alert(`Salário:R$ ${salario}`)
+
+//}
+//calcularSalario(60, 20, 350)
+
+//function calcularAreadoTriangulo(base, altura){
+//let area = base * altura / 2
+//alert(`ÁreadoTriangulo: ${area}`)
+//}
+//calcularAreadoTriangulo(15, 30)
+
+//Escreva uma função com dois parametros (peso e altura) e calcule o imc, exiba o resultado
+//imc = peso / altura²
+
+//function calcularImc(peso, altura) {
+  //  let imc = peso / altura ** 2
+    //alert(imc)
+//}
+
+//calcularImc(82, 1.81)
+
+//Escreva uma função que recebe um parametro para um valor em real e converta-o para dolar, considerando a cotação 5.45
+
+//function converterMoeda(real) {
+  //  let dolar = real / 5.45
+   //alert(dolar)
+//}
+
+//converterMoeda(100)
+
+//Escreva uma função que recebe base menor, base maior e altura por parametro, calcule a area de um trapezio e exiba o resultado
+//area = (base maior + base menor) x altura / 2
+
+//function calcularTrapezio(baseMaior, baseMenor, altura) {
+    //let area = (baseMaior + baseMenor) * altura / 2
+  //  alert(area)
+//}
+
+//calcularTrapezio(10, 5, 8)
+
+//Escreva uma função que recebe o consumo de combustivel por parametro e informe quanto ele gastou, considerando o valor do litro 6.45
+
+//function calcularGastos(consumo) {
+  //  let gasto = consumo * 6.45
+    //alert(gasto)
+//}
+//calcularGastos(10)
+
+// function somar(num1, num2){
+// let resultado = num1 + num2
+// return resultado
+
+// }
+// let res= somar(15, 19)
+// alert(res)
+// alert(somar(15, 19))
+
+// function calcularMedia(n1, n2, n3){
+//     const media = (n1 + n2 + n3) / 3
+//     return media
+// }
+// const resultado = calcularMedia(10, 9,5, 7)
+// alert(resultado)
+
+//Escreva uma função que recebe uma temperatura em celsius por parametro e retorna o resultado em fahrenheit
+//f = (celsius x 9/5) + 32
+
+// function calcularDistancia(velocidade, tempo){
+// const distancia= (velocidade * tempo)
+// return distancia
+// }
+// const resultado= calcularDistancia(100, 15)
+// alert(resultado)
+
+//  Escreva uma função que recebe uma temperatura em celsius por parametro e retorna o resultado em fahrenheit
+// f = (celsius x 9/5) + 32
+
+// function converterTemperatura(celsius) {
+//     const f = (celsius * 9 / 5) + 32
+//     return f
+// }
+
+// const resultado = converterTemperatura(32)
+// alert(resultado)
